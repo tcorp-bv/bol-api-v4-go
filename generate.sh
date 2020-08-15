344935dc412fdd3923d7d5cd02792f89781af727
toonsevrin <twan123@live.be>
2020-08-15 13:29:37 +0200

    deprivileged by default

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo rm -rf openapi
-sudo docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate -i /local/types.json -g go -o /local/openapi
-sudo rm openapi/go.mod openapi/go.sum
+rm -rf openapi
+docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate -i /local/types.json -g go -o /local/openapi
+rm openapi/go.mod openapi/go.sum
